9e3bf5d8706d10069170345b6930a0abec842ed9
Stanwang1210 <55189574+Stanwang1210@users.noreply.github.com>
2022-01-12 15:51:05 +0800

    Add files via upload

--- Person.py
+++ Person.py
@@ -9,13 +9,15 @@ class Person:
             "pic_path" : str,
             "Age" : int
         }
-
+        os.makedirs("user_data", exist_ok=True)   
     def register(self, name, age, pic_path):
+        
         self.data = {
             "name" : name,
             "pic_path" : pic_path,
             "Age" : age
         }
         file_name = self.data["name"]
-        json.dumps(self.data, join("./Auth", f"{file_name}.json"))
+        with open(join("./Auth", f"{file_name}.json"), "w") as f:
+            json.dump(self.data, f)
 
